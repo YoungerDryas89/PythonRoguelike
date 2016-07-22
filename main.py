fe01c1d76244ef64cfa64d227a326795d4491b36
saberman888 <saberman8881@gmail.com>
2016-07-21 22:02:20 -0600

    Add files via upload
    Dungeon Generator work in progress

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 #Abstract Entity
 class Entity:
     def __init__(self, ascii, x, y):
@@ -26,10 +27,11 @@ class Tile(PerminantEntity):
 
 
 class NPC(Entity):
-    def __init__(self, ascii, x, y):
+    def __init__(self, ascii, x, y, hp):
         self.ascii = ascii
         self.x = x
         self.y = y
+        self.hp = hp
 
     def Move(self, dx, dy, nmap, tmap):
         if tmap[self.y+dy][self.x+dx] is not None and tmap[self.y+dy][self.x+dx].blocked == True:
@@ -70,11 +72,22 @@ class Door(Tile):
             #self.blocked_sight = False
             self.ascii = "!"
             self.status = 1
+            
+class Exit(Tile):
+    def __init__(self):
+        Tile.__init__(self, ">", False, False)
 
+    def Activate(self, i):
+        self.i.status = 1
+        
 class Player(NPC):
-    def __init__(self, ascii, x, y):
-        NPC.__init__(self, ascii, x, y)
-
+    def __init__(self, ascii, x, y, hp):
+        NPC.__init__(self, ascii, x, y, hp)
+        self.status = 0
+        #Statuses
+        # 0 = In-Game
+        # 1 = Exited
+        
     def handle_input(self, i, nmap, tmap):
         i = i.upper()
         if i == "N":
@@ -86,6 +99,12 @@ class Player(NPC):
         elif i == "W":
             self.Move(1, 0, nmap, tmap)
     
+
+class Enemy(NPC):
+    def __init__(self, name, ascii, x, y, hp):
+        NPC.__init__(self, ascii, x, y, hp)
+        self.name = name
+
         
 
 class Map:
@@ -94,6 +113,7 @@ class Map:
         self.width = width
         self.tile_map = [[ Wall() for x in range(0, self.width)] for y in range(0, self.height)]
         self.npc_map = [[ None for x in range(0, self.width)] for y in range(0, self.height)]
+        self.npc = []
 
     def clear_space(self, xpoint, ypoint, height, width):
         for y in range(ypoint, ypoint+height):
@@ -173,14 +193,50 @@ class Map:
         for y in range(y, y+length):
             self.tile_map[y][x] = None
 
+    def GenerateDungeon(self):
 
+        #defined constants
+        min_size_room = 5
+        max_size_room = 25
+        
+        # Get starting point for first room or spawning room
+        starting_y = random.randint(0, self.height)
+        starting_x = random.randint(0, self.width)
+        
+        #Calculate the size of the room
+        starting_height = random.randint(min_size_room, max_size_room)
+        starting_width = random.randint(min_size_room, max_size_room)
+        
+        # Create the first room or known as the spawn room
+        self.clear_space(starting_x, starting_y, starting_width, starting_height)
+
+
+    def SquareCamera(self, ascii_map, P):
+        px = P.x
+        py = P.y
+        return_map = [[ " " for x in range(0, px)] for y in range(0, py)]
+
+        
+        if px == self.width or py == self.height:
+            if px == self.width:
+                pass
+        for y in range(py, py+py):
+            ypoint = ascii_map[y]
+            for x in range(px, px+px):
+                xpoint = ypoint[x]
+                return_map[y][x] = xpoint
+
+        return return_map
+        
 if __name__ == "__main__":
-    M = Map(20, 20)
+    M = Map(50, 50)
     #D = Door()
-    P = Player("@", 8, 5)
+    E = Exit()
+    M.AddTile(E, 17, 5)
+    P = Player("@", 8, 5, 100)
     M.AddNPC(P)
     #M.AddTile(D, 3, 4)
-    M.clear_space(5, 5, 5, 5)
+    M.clear_space(5, 5, 30, 30)
     npc = [P]
 
     M.CreateVerticalLine(10, 7, 5)
